85f2f08179de75b6466a89e62f5f03e08a15424f
Alexsander <sparco@nxt.ru>
2024-04-16 21:11:44 +0500

    import_export dz done

---
+++ import_export/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: path.resolve(__dirname, './src/index.html')
+            }
+        ),
+        new MiniCssExtractPlugin(
+            {
+                filename: 'main.css',
+            }
+        )
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            }
+        ]
+    }
+}
